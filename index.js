62abf5746d6b044b706b05df136471168839ca72
Tekalig <tekahazi06@gmail.com>
2024-05-11 15:10:05 +0300

    completed index.html, styles.css and index.js file

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
 let input = document.querySelector(".display");
+let result = 0;
+let valueHold = 0;
+let oprator = "";
+input.textContent = valueHold;
 document.querySelectorAll(".number").forEach((num) => {
-  num.onclick = () => {
-    input.textContent += num.textContent.trim();
-    console.log(input.textContent.trim());
-  };
+  num.addEventListener("click", () => {
+    if (valueHold === 0 || valueHold === result) {
+      valueHold = "";
+    }
+    valueHold += num.textContent.trim();
+    input.textContent = valueHold;
+    valueHold = Number(valueHold);
+  });
 });
+
+document.querySelectorAll(".oprator").forEach((op) => {
+  op.addEventListener('click', () => {
+    if (oprator) {
+      result = eval(result + oprator + valueHold);
+    }
+    else {
+      result = valueHold;
+    }
+    oprator = op.textContent.trim()[0];
+    input.textContent = "";
+    valueHold = 0;
+  });
+});
+
+document.querySelector(".plus_minus").addEventListener('click', () => {
+  if (valueHold !== 0) {
+    valueHold *= -1;
+    input.textContent = valueHold;
+  }
+});
+
+document.querySelector(".percentage").addEventListener("click", () => {
+  valueHold /= 100;
+  input.textContent = valueHold;
+});
+
+document.querySelector(".dot").addEventListener("click", () => {
+  if (!valueHold.toString().includes(".")) {
+    valueHold += ".";
+    input.textContent = valueHold;
+  }
+});
+
+clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+  input.textContent = 0;
+  result = 0;
+  valueHold = 0;
+  oprator = '';
+});
+
+document.querySelector(".equal").addEventListener("click", () => {
+  if (oprator) {
+    result = eval(result + oprator + valueHold);
+    input.textContent = result;
+    oprator = "";
+    valueHold = result;
+  }
+})
